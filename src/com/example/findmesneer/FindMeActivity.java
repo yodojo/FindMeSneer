4d42d8e161b2f48488bc637ccce0c37c3960fafa
Leonardo Souza de Assis <gnomolsa@gmail.com>
2014-03-22 18:01:55 -0400

    Implementada a localização a partir do gps

--- src/com/example/findmesneer/FindMeActivity.java
+++ src/com/example/findmesneer/FindMeActivity.java
@@ -1,9 +1,19 @@
 package com.example.findmesneer;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.ExecutionException;
+
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,8 +21,23 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class FindMeActivity extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.internal.ex;
+import com.google.android.gms.location.LocationClient;
+
+public class FindMeActivity extends Activity implements
+		GooglePlayServicesClient.ConnectionCallbacks,
+		GooglePlayServicesClient.OnConnectionFailedListener {
+
+	private LocationClient mLocationClient;
+	
+	private static TextView mEndereco;
+
+	private String geoUriString = "geo:15.5555,16.5000?q=(rua a)@15.5555,16.5000";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,38 +49,20 @@ public class FindMeActivity extends Activity {
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 		
+
+		mLocationClient = new LocationClient(this, this, this);
+
 	}
 
-	public void abrirMapa(View view)
-	{
-		String geoUriString="geo:15.5555,16.5000?q=(rua a)@15.5555,16.5000";
+	public void abrirMapa(View view) {
+		// geoUriString="geo:15.5555,16.5000?q=(rua a)@15.5555,16.5000";
 		Uri geoUri = Uri.parse(geoUriString);
-		
-		Intent mapCall  = new Intent(Intent.ACTION_VIEW, geoUri);
+
+		Intent mapCall = new Intent(Intent.ACTION_VIEW, geoUri);
 		startActivity(mapCall);
 	}
-	
-	
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
 
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.find_me, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	
 
 	/**
 	 * A placeholder fragment containing a simple view.
@@ -70,8 +77,145 @@ public class FindMeActivity extends Activity {
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_find_me,
 					container, false);
+			mEndereco  = (TextView) rootView.findViewById(R.id.textView1);
 			return rootView;
 		}
+	}
+
+	private void atualizaPosicao() {
+		Location mCurrentLocation = mLocationClient.getLastLocation();
+		
+		
+			AsyncTask<Location,Void,String> execute = (new GetAddressTask(this)).execute(mCurrentLocation);
+			
+			try {
+			mEndereco.setText(execute.get());
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			geoUriString = "geo:" + mCurrentLocation.getLatitude() + ","
+					+ mCurrentLocation.getLongitude() + "?q=("+mEndereco.getText()+")@"
+					+ mCurrentLocation.getLatitude() + ","
+					+ mCurrentLocation.getLongitude();
+			
+	
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		// Connect the client.
+		mLocationClient.connect();
+	
+	}
+
+	@Override
+	protected void onStop() {
+		// Disconnecting the client invalidates it.
+		mLocationClient.disconnect();
+		super.onStop();
+	}
+
+	@Override
+	public void onConnected(Bundle dataBundle) {
+		// Display the connection status
+		Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+		atualizaPosicao();
+
+	}
+
+	/*
+	 * Called by Location Services if the connection to the location client
+	 * drops because of an error.
+	 */
+	@Override
+	public void onDisconnected() {
+		// Display the connection status
+		Toast.makeText(this, "Disconnected. Please re-connect.",
+				Toast.LENGTH_SHORT).show();
+	}
+
+	/*
+	 * Called by Location Services if the attempt to Location Services fails.
+	 */
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+
+		Toast.makeText(this, "Connection Failed", Toast.LENGTH_SHORT).show();
+
+	}
+
+	private class GetAddressTask extends AsyncTask<Location, Void, String> {
+		Context mContext;
+
+		public GetAddressTask(Context context) {
+			super();
+			mContext = context;
+		}
+
+
+		@Override
+		protected String doInBackground(Location... params) {
+			Geocoder geocoder = new Geocoder(mContext, Locale.getDefault());
+			// Get the current location from the input parameter list
+			Location loc = params[0];
+			// Create a list to contain the result address
+			List<Address> addresses = null;
+			try {
+				/*
+				 * Return 1 address.
+				 */
+				addresses = geocoder.getFromLocation(loc.getLatitude(),
+						loc.getLongitude(), 1);
+			} catch (IOException e1) {
+				Log.e("LocationSampleActivity",
+						"IO Exception in getFromLocation()");
+				e1.printStackTrace();
+				return ("IO Exception trying to get address");
+			} catch (IllegalArgumentException e2) {
+				// Error message to post in the log
+				String errorString = "Illegal arguments "
+						+ Double.toString(loc.getLatitude()) + " , "
+						+ Double.toString(loc.getLongitude())
+						+ " passed to address service";
+				Log.e("LocationSampleActivity", errorString);
+				e2.printStackTrace();
+				return errorString;
+			}
+			// If the reverse geocode returned an address
+			if (addresses != null && addresses.size() > 0) {
+				// Get the first address
+				Address address = addresses.get(0);
+				/*
+				 * Format the first line of address (if available), city, and
+				 * country name.
+				 */
+				String addressText = String.format(
+						"%s, %s, %s",
+						// If there's a street address, add it
+						address.getMaxAddressLineIndex() > 0 ? address
+								.getAddressLine(0) : "",
+						// Locality is usually a city
+						address.getSubAdminArea(),
+						// The country of the address
+						address.getCountryName());
+				// Return the text
+				return addressText;
+			} else {
+				return "No address found";
+			}
+		}
+		
+		   protected void onPostExecute(String address) {
+	            // Display the results of the lookup.
+			   if(address!=null && mEndereco !=null)
+			   mEndereco.setText(address);
+	        }
 	}
 
 }
